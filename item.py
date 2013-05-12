c1587097a8b950cc44bc5bcbd1f2538df4a3ae72
CKnotts <christina.knotts@gmail.com>
2013-05-12 12:15:30 -0600

    Update item.py

--- item.py
+++ item.py
@@ -21,22 +21,16 @@ def create_item(rdb):
     iname = request.POST.get('item_name','').strip()
     icost = request.POST.get('item_cost','').strip()
     inotes = request.POST.get('item_notes','').strip()
+    istatus = request.POST.get('status','').strip()
 
     #get current user info
     user = request.get_cookie('account', secret='pass')
     user_id = str(int(rdb.zscore('accounts:usernames', user)))
-
-    #item_id = current number of task items +1
-    #   Options:
-    #       1:  item_id = str(rdb.hincrby('task:' + user_id + ':' + event_id + ':' + task_id, 'numitems',1))
-    #           Can use hincrby to increment the number of event tasks before
-    #           update takes place. If task insertion fails, will need to
-    #           decrement number of event tasks.
-    #       2:  item_id = int(rdb.hget('task:' + user_id + ':' + event_id + ':' + task_id, 'numitems')) + 1
-    #           Can retrieve the number of event tasks, then increment the
-    #           result by 1. If task insertion is successful, will need to
-    #           increment the number of event tasks.
     
+    #Create new item_id
+    item_id = int(rdb.hget('task:' + user_id + ':' + event_id + ':' + task_id, 'numitems')) + 1
+
+
     item_key = 'item:%s:%s:%s' % (user_id, event_id, task_id, item_id)
 
     #Try to add the new item to the database.
@@ -45,12 +39,9 @@ def create_item(rdb):
                  { 'iname' : iname,
                    'icost' : icost,
                    'inotes' : inotes,
-                   'istatus' : constants.getStatusIntFromStr(tstatus) })
+                   'istatus' : constants.getStatusIntFromStr(istatus) })
+        rdb.hset('task:' + user_id + ':' + event_id + ':' + task_id, 'numitems', item_id)
     except:
-        #TODO: error item insertion failed.
-        #If hincrby was used, need to undo:
-        #ino = int(item_id) - 1
-        #rdb.hset('task:' + user_id + ':' + event_id + ':' + task_id, { 'numitems' : ino} )
         return None
 
     return (user_id,  event_id)
