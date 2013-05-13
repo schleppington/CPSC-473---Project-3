eb186d3cd7c7f25634a940b187858874c2eee213
CKnotts <christina.knotts@gmail.com>
2013-05-12 20:06:38 -0600

    Update planner.py
    Bug Squashed: Redirected to '/event/%s/%s/' when it should have been '/event/%s/%s' . This caused an error 500 to be displayed since the view was not found.

--- planner.py
+++ planner.py
@@ -177,7 +177,7 @@ def newEvent_submit(rdb):
     result = event.create_event(rdb)
     #   result = (user_id , event_id)
     if result:
-        redirect('/event/%s/%s/' % result)
+        redirect('/event/%s/%s' % result)
     #event created
     else:
         #failed to create event
